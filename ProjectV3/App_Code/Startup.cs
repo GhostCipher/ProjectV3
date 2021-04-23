cd2957d4b4ec3e93bead03afd6b14f2a4ced3a45
GhostCipher <47286235+GhostCipher@users.noreply.github.com>
2021-04-22 23:23:46 -0500

    Add project files.

---
+++ ProjectV3/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProjectV3.Startup))]
+namespace ProjectV3
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
